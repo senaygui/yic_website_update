8b67478351f6847ffa9cda6ea9e121ce2d1ff41c
senaygui <senaygeletaw@gmail.com>
2021-10-07 10:19:34 +0300

    capstroano config

--- Gemfile
+++ Gemfile
@@ -61,6 +61,11 @@ group :development, :test do
 end
 
 group :development do
+  gem 'capistrano', '~> 3.11'
+  gem 'capistrano-rails', '~> 1.4'
+  gem 'capistrano-passenger', '~> 0.2.0'
+  gem 'capistrano-rbenv', '~> 2.1', '>= 2.1.4'
+  gem 'capistrano-rails-db'
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
   gem 'web-console', '>= 3.3.0'
   gem 'listen', '>= 3.0.5', '< 3.2'
---
+++ config/deploy.rb
@@ -0,0 +1,49 @@
+# config valid for current version and patch releases of Capistrano
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.16.0"
+
+set :application, "yic_website"
+set :repo_url, "https://github.com/senaygui/yic_website.git"
+# set :ssh_options, { :forward_agent => true, :port => 4321 }
+set :user, "deploy"
+set :passenger_restart_with_touch, true
+# set :rbenv_path, '/home/deploy/.rbenv/'
+set :stages, %w(production staging)
+# append :linked_files, "config/database.yml", "config/master.key"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
+
+set :keep_releases, 5
+set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
